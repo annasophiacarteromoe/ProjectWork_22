e69fcefb60728923166adb1cb29584b848035255
OskierP <pakooskar@gmail.com>
2022-11-14 14:42:02 +0100

    Update pass-array.service.ts

--- medic_assist/src/app/PrescriptionService/pass-array.service.ts
+++ medic_assist/src/app/PrescriptionService/pass-array.service.ts
@@ -13,14 +13,14 @@ export class PassArrayService {
   showMedsArray():void {
     console.log(this.meds2prescriptionArray)
   }
-  
+
   addMed(med:string):void{
     this.meds2prescriptionArray.push(med)
   }
 
-  deleteMed(med:string):void{
-    this.meds2prescriptionArray.forEach((value,index)=>{
-      if(value==med) this.meds2prescriptionArray.splice(index,1);
-    });
-  }
+  // deleteMed(med:string):void{ //could be used to delete
+  //   this.meds2prescriptionArray.forEach((value,index)=>{
+  //     if(value==med) this.meds2prescriptionArray.splice(index,1);
+  //   });
+  // }
 }
